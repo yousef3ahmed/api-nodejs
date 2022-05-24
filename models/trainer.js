33df9abfa62377d9091cda6e9e1fc85b4227f256
Heba Elsayed <93211834+heba-els@users.noreply.github.com>
2022-05-24 23:27:00 +0200

    add trainer

---
+++ models/trainer.js
@@ -0,0 +1,36 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const Trainer = mongoose.model('Trainer', new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 255,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 1024
+    }
+}));
+
+function validateUser(trainer) {
+    const schema = {
+        name: Joi.string().min(5).max(50).required(),
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(5).max(255).required()
+    };
+    return Joi.validate(trainer, schema);
+}
+
+exports.Trainer = Trainer;
+exports.validate = validateUser;
